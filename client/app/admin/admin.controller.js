e1ec2ed809f6d79abf826e6ac81c6ec02c166633
Alvaro Almendros Gala <alvaroalmendros@me.com>
2016-01-10 01:21:17 +0100

    Admin infoclan editor

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -3,9 +3,33 @@
 (function() {
 
 class AdminController {
-  constructor(User) {
+  constructor(User, $http, $scope, socket) {
     // Use the User $resource to fetch all users
     this.users = User.query();
+
+    // Infoclan
+    this.$http = $http;
+    this.nombre = "";
+    this.twitter = "";
+    this.texto = "";
+
+    $http.get('/api/infoclan').then(response => {
+      this.nombre = response.data.nombre;
+      this.twitter = response.data.twitter;
+      socket.syncUpdates('nombre', this.nombre);
+      socket.syncUpdates('twitter', this.nombre);
+    });
+
+    $http.get('/api/infoclan/texto').then(response => {
+      this.texto = response.data.texto;
+      socket.syncUpdates('texto', this.texto);
+    });
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('nombre');
+      socket.unsyncUpdates('twitter');
+      socket.unsyncUpdates('texto');
+    });
   }
 
   delete(user) {
@@ -13,6 +37,8 @@ class AdminController {
     this.users.splice(this.users.indexOf(user), 1);
   }
 }
+
+
 
 angular.module('eecrApp.admin')
   .controller('AdminController', AdminController);
