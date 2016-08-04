56ed38dfb375365cf8d4187b04336a60a13d855e
Alvaro Almendros Gala <alvaroalmendros@me.com>
2016-08-04 17:20:19 +0200

    [FIX] textangular sanitize config

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -1,9 +1,14 @@
 'use strict';
 
-export function routeConfig($urlRouterProvider, $locationProvider) {
+export function routeConfig($urlRouterProvider, $locationProvider, $provide) {
   'ngInject';
 
   $urlRouterProvider.otherwise('/');
 
   $locationProvider.html5Mode(true);
+
+  $provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions) {
+    taOptions.forceTextAngularSanitize = false;
+    return taOptions;
+  }]);
 }
