5452ac48ad17534b65a07025f99d68fcf67ad907
Alvaro Almendros Gala <alvaroalmendros@me.com>
2016-08-04 16:28:31 +0200

    [FIX] footer y navbar

--- client/components/navbar/navbar.component.js
+++ client/components/navbar/navbar.component.js
@@ -1,21 +1,44 @@
 'use strict';
+import angular from 'angular';
+
 
 export class NavbarComponent {
   menu = [{
-    'title': 'Home',
-    'state': 'main'
+    title: 'Home',
+    state: 'main'
   }];
   isLoggedIn: Function;
   isAdmin: Function;
   getCurrentUser: Function;
   isCollapsed = true;
 
-  constructor(Auth) {
+  constructor($scope, Auth, infoclan, $location) {
     'ngInject';
 
+    this.$location = $location;
     this.isLoggedIn = Auth.isLoggedInSync;
     this.isAdmin = Auth.isAdminSync;
     this.getCurrentUser = Auth.getCurrentUserSync;
+    this.user = Auth.getCurrentUser();
+
+    this.infoclan = infoclan;
+    infoclan.get().then();
+
+    $scope.status = {
+      isopen: false
+    };
+  }
+
+  isColider() {
+    if (this.user.rango === 'colider' || this.user.rango === 'lider') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isActive(route) {
+    return route === this.$location.path();
   }
 
 }
