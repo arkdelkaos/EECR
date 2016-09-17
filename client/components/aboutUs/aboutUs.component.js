34d274d689db6cb35d7dbe35a73c22b6eb2b5bb2
Alvaro Almendros Gala <alvaroalmendros@me.com>
2016-09-17 21:29:57 +0200

    feat : Discord widget, custom
    Añado el widget de discord, custom desde la api->json, sacabdo el diseño del widget oficial, pero a mi bola ;)
    
    [ADD] Discord Widget
    (FIX) Normas

--- client/components/aboutUs/aboutUs.component.js
+++ client/components/aboutUs/aboutUs.component.js
@@ -4,11 +4,18 @@ export class aboutUsComponent {
 
   isAdmin: Function;
 
-  constructor(Auth, infoclan) {
+  constructor(Auth, infoclan, $http, $log) {
     'ngInject';
     this.isAdmin = Auth.isAdminSync;
     this.infoclan = infoclan;
     infoclan.get().then();
+
+    this.log = $log;
+    this.discord = {};
+    $http.get('https://discordapp.com/api/servers/142056318889361408/widget.json').then(response => {
+      this.discord = response.data;
+      this.log.info(this.discord);
+    });
   }
 
   save() {
